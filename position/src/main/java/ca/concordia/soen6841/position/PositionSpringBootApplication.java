e2cd7b4abb716d7a18ac9a35a82e44f2e309d5d9
abhin <abhinandanchivate2000@gmail.com>
2020-12-01 17:53:16 +0530

    position

---
+++ position/src/main/java/ca/concordia/soen6841/position/PositionSpringBootApplication.java
@@ -0,0 +1,13 @@
+package ca.concordia.soen6841.position;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableEurekaClient
+public class PositionSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PositionSpringBootApplication.class, args);
+	}
+}
