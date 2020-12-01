608d9add029318e61f38f546d7857f9546b6109e
abhin <abhinandanchivate2000@gmail.com>
2020-12-01 15:13:52 +0530

    auth service

---
+++ auth/src/main/java/ca/concordia/soen6841/auth/AuthSpringBootApplication.java
@@ -0,0 +1,15 @@
+package ca.concordia.soen6841.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+//@EnableEurekaClient
+public class AuthSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthSpringBootApplication.class, args);
+	}
+}
